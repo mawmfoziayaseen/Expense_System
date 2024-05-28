28ac17a89dd13174dc06a006fd94f4cc2d532f59
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-05-28 23:30:20 +0500

    detele code correction

--- delete.expense.php
+++ delete.expense.php
@@ -7,21 +7,18 @@ if (isset($_REQUEST['del_expense_id'])) {
     $del_expense_id = $_REQUEST['del_expense_id'];
     $sql = "DELETE FROM `expense_info` WHERE `item_id`='$del_expense_id'";
     $result = mysqli_query($conn, $sql);
-}
-if ($result) {
-    my_alert("success", "expense deleted successfully");
-    header("Location:./all_expense.php");
-} else {
-    my_alert("danger", "expense not deleted");
-}
 
-
+    if ($result) {
+        my_alert("success", "Expense deleted successfully");
+        header("Location: ./all_expense.php");
+        exit(); // Ensure no further code is executed after redirection
+    } else {
+        my_alert("danger", "Expense not deleted");
+    }
+}
 
 mysqli_close($conn);
-
-
 ?>
-
 
 <?php
 include('./include/footer.php');
--- delete_user.php
+++ delete_user.php
@@ -3,25 +3,24 @@ include('./include/header.php');
 include('./include/function.php');
 include('./include/db_conn.php');
 
+$result = false; // Initialize $result to avoid undefined variable error
+
 if (isset($_REQUEST['delete_id'])) {
     $delete_id = $_REQUEST['delete_id'];
     $sql = "DELETE FROM `reg_users` WHERE `reg_id`='$delete_id'";
     $result = mysqli_query($conn, $sql);
-}
-if ($result) {
-    my_alert("success", "user deleted successfully");
-    header("Location:./display_reg_users.php");
-} else {
-    my_alert("danger", "user not deleted");
-}
 
-
+    if ($result) {
+        my_alert("success", "User deleted successfully");
+        header("Location: ./display_reg_users.php");
+        exit(); // Ensure no further code is executed after redirection
+    } else {
+        my_alert("danger", "User not deleted");
+    }
+}
 
 mysqli_close($conn);
-
-
 ?>
-
 
 <?php
 include('./include/footer.php');
